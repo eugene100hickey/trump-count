9d5cef0437ae9bfded0427783d028b4e611dc934
eugene100hickey <eugene.hickey@ittdublin.ie>
2021-03-17 19:42:23 +0000

    before annotations

--- trump-count.R
+++ trump-count.R
@@ -4,6 +4,17 @@ library(stringi)
 require(handlr)
 library(lubridate)
 library(zoo)
+library(ggtext)
+library(showtext)
+
+font_add(family = "lemonada", regular = "fonts/lemonada/static/Lemonada-Regular.ttf")
+font_add(family = "GOT", regular = "fonts/Game-of-Thrones/Game of Thrones.ttf")
+font_add(family = "Nanum", regular = "fonts/NanumMyeongjo/NanumMyeongjo-Regular.ttf")
+font_add(family = "dark11", regular = "fonts/Dark11/DARK11__.ttf")
+font_add(family = "Kalam", regular = "fonts/Kalam/Kalam-Regular.ttf")
+font_add(family = "drakon", regular = "fonts/drakon/drakon.ttf")
+
+showtext_auto()
 
 # ev_mentions <- function(url) {
 #   w <- read_html(url)
@@ -31,7 +42,7 @@ library(zoo)
 
 z <- readRDS("data/all-z-plus-missing")
 
-z <- z %>% mutate(quarters = quarters(date))
+# z <- z %>% mutate(quarters = quarters(date))
 # 
 # missing <- z %>% 
 #   filter(length < 200)
@@ -70,35 +81,60 @@ z1 <- z %>%
   select(-length)
 
 z2 <- z1 %>% 
-  pivot_longer(-c(date, trump, biden, clinton, quarters), 
+  pivot_longer(-c(date, trump, biden, clinton), 
                names_to = "candidate_7", 
                values_to = "mentions_7") %>% 
   select(-c(trump, clinton, biden))
 
 z3 <- z1 %>% 
-  pivot_longer(-c(date, trump_7, biden_7, clinton_7, quarters), 
+  pivot_longer(-c(date, trump_7, biden_7, clinton_7), 
                names_to = "candidate", 
                values_to = "mentions") %>% 
   select(-c(trump_7, clinton_7, biden_7)) %>% 
   left_join(z2)
 
+colour_stripe <- c("grey20", "grey30", "grey40", "grey50")
+date_range_matrix <- matrix(as.numeric(seq.Date(from = min(z$date), 
+                                                to = max(z$date), by = "quarter")), 
+                            ncol = 2, byrow = TRUE)
+date_range_df <- tibble::tibble(start = zoo::as.Date.numeric(date_range_matrix[,1]), 
+                                end = zoo::as.Date.numeric(date_range_matrix[, 2]))
+date_breaks <- c(date_range_df$start, date_range_df$end)
+candidate_colours <- c("#bfd200", "#046c9a", "#972D15")
+plot_title <- glue::glue("Daily EV Mentions for <i style= 'font-family: forum; color:{candidate_colours[1]}; font-size: 40px;'>Clinton</i>, ",
+                         "<i style='font-family: Kalam; color:{candidate_colours[2]}; font-size: 40px;'>Biden</i>, ",
+                         "and <b><i style = 'font-family:drakon; color:{candidate_colours[3]}; font-size: 40px;'>Trump</i></b>")
+
 z3 %>% ggplot(aes(date, mentions, colour = candidate)) + 
-  geom_line(aes(y=mentions_7, colour = candidate_7), show.legend = F) +
-  scale_color_manual(values = c(clinton_7 = "green", biden_7 = "blue", trump_7 = "red")) +
-  geom_point(data = z1, aes(y = trump), colour = "red", size = 0.2, alpha = 0.2) +
+  geom_line(aes(y=mentions_7, colour = candidate_7), show.legend = F, size = 1.5) +
+  scale_color_manual(values = c(clinton_7 = candidate_colours[1], 
+                                biden_7 = candidate_colours[2], 
+                                trump_7 = candidate_colours[3])) +
+  scale_x_yearqtr(breaks = date_breaks, 
+                  lab = format(date_breaks, 
+                               ifelse(month(date_breaks) == 11, "%b\n        %Y", "%b"))) +
+  geom_point(data = z1, aes(y = trump), 
+             colour = candidate_colours[3], 
+             size = 0.5, 
+             alpha = 0.5) +
   theme_minimal() +
-  geom_rect(data = date_range_df, ggplot2::aes(xmin = start, xmax = end, 
-                                               ymin = -Inf, ymax = Inf), 
+  geom_rect(data = date_range_df, 
+            aes(xmin = start, xmax = end,
+                ymin = -Inf, ymax = Inf), 
             inherit.aes = FALSE,
-            alpha = 0.4, 
-            fill = colour_stripe)
+            alpha = 0.4,
+            fill = "grey90") +
+  coord_cartesian(ylim = c(0, 80)) +
+  labs(title = plot_title,
+       x = "",
+       y = "",
+       caption = glue::glue("<i><span style = 'color:{candidate_colours[3]};'> &bull; </span></i>'s are Trump daily counts, lines are 7 day rolling means")) +
+  theme(plot.title = element_markdown(size = 28, hjust = 0.5),
+        text = element_text(family = "Ink Free", size = 18),
+        plot.caption = element_markdown())
 
  saveRDS(z, "data/all-z-plus-missing")
 
- colour_stripe <- "lightgray"
- date_range_matrix <- matrix(as.numeric(seq.Date(from = min(z$date), 
-                                                 to = max(z$date), by = "quarter")), 
-                             ncol = 2, byrow = TRUE)
- date_range_df <- tibble::tibble(start = zoo::as.Date.numeric(date_range_matrix[, 
-                                                                                1]), end = zoo::as.Date.numeric(date_range_matrix[, 2]))
+
+
  
